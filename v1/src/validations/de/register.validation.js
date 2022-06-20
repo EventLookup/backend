13cd8edea90b12598eab1181eb15c686bb908027
Dietmar E <86421371+Dima0687@users.noreply.github.com>
2022-06-20 09:44:41 +0200

    Jerry: added register validation
    Co-authored-by: Jerry <jerry.taag@gmail.com>

---
+++ v1/src/validations/de/register.validation.js
@@ -0,0 +1,34 @@
+import { body } from 'express-validator';
+
+export const registerValidationSchema = [
+    body('email')
+        .not()
+        .isEmpty()
+        .withMessage('Pflichtfeld! Bitte fülle dieses Feld aus.')
+        .trim()
+        .isEmail()
+        .withMessage('Bitte gib eine gültige Email-Adresse an.')
+        .normalizeEmail(),
+
+    body('username')
+        .not()
+        .isEmpty()
+        .withMessage('Pflichtfeld! Bitte fülle dieses Feld aus.')
+        .trim()
+        .isLength({min: 3, max: 50})
+        .withMessage('Der Username muss muss zwischen 3 und 50 Zechen lang sein.')
+        .not()
+        .matches(/\W/)
+        .withMessage('Der Username darf keine Sonderzeichen enthalten.'),
+
+    body('password')
+        .not()
+        .isEmpty()
+        .withMessage('Pflichtfeld! Bitte fülle dieses Feld aus.')
+        .trim()
+        .isLength({min: 10})
+        .withMessage('Das Passwort muss mindestens 10 Zeichen lang sein.')
+        .isStrongPassword()
+        .withMessage('Das Passwort muss folgendes beinhalten: min. 1 Großbuschstaben, min. 1 Kleinbuschstaben, min. 1 Zahl, min. 1 Sonderzeichen.')
+        .escape()
+]
