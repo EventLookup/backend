9be283c7471a8f82ca07ad0dcb842eda066de825
Jerry T <89188588+Aypoleyth@users.noreply.github.com>
2022-06-20 12:29:13 +0200

    Jerry: erstellt validate userprofile
    * userprofile validation added
    
    * houseNr edited
    
    * houseNr edited

--- v1/src/validations/de/userprofile.validation.js
+++ v1/src/validations/de/userprofile.validation.js
@@ -31,16 +31,20 @@ export const userprofileValidationSchema = [
 
     body('street')
         .trim()
-        .not()
+        // .not()
         // Wirklich Zahlen rausfiltern? Es gibt Straßen mit dem Namen 'Straße 442' oder anderer Zahl
         // .matches(/\d/)
         // .withMessage('Der Straßenname darf keine Zahlen enthalten.')
         .isLength({max: 50}),
 
-    body('houseNumber')
+    body('houseNr')
         .trim()
         .isLength({min: 1, max: 3})
-        .withMessage('Die Hausnummer muss zwischen 1 und 999 sein.')
+        .withMessage('Die Hausnummer muss zwischen 1 und 999 sein.'),
+        // Nicht filtern weil es noch Zusätze gibt wie z.B.: 11b
+        // .not()
+        // .matches(/\D/)
+        // .withMessage('Die Hausnummer darf nur aus Zahlen bestehen.')
         .not()
         .matches(/\D/)
         .withMessage('Die Hausnummer darf nur aus Zahlen bestehen.'),
