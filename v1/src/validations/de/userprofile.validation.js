fcda1f2263caeab93f378bc32ea7e2dda7fd6c99
Jerry <jerry.taag@gmail.com>
2022-06-20 09:41:04 +0200

    userprofile validation added

---
+++ v1/src/validations/de/userprofile.validation.js
@@ -0,0 +1,63 @@
+import { body } from 'express-validator';
+
+export const userprofileValidationSchema = [
+    body('firstname')
+        .trim()
+        .not()
+        .matches(/\d/)
+        .withMessage('Der Vorname darf keine Zahlen enthalten.')
+        .not()
+        .matches(/\W/)
+        .withMessage('Der Vorname darf keine Sonderzeichen enthalten.')
+        .escape,
+        
+    body('lastname')
+        .trim()
+        .not()
+        .matches(/\d/)
+        .withMessage('Der Nachname darf keine Zahlen enthalten.')
+        .not()
+        .matches(/\W/)
+        .withMessage('Der Nachname darf keine Sonderzeichen enthalten.')
+        .escape,
+
+    body('organizer')
+        .not()
+        .isEmpty()
+        .withMessage('Organizer muss auf true oder false gesetzt sein.')
+        .trim()
+        .matches(/^true$|^false$/)
+        .withMessage('Der Wert muss "true" oder "false" sein.'),
+
+    body('street')
+        .trim()
+        .not()
+        // Wirklich Zahlen rausfiltern? Es gibt Straßen mit dem Namen 'Straße 442' oder anderer Zahl
+        // .matches(/\d/)
+        // .withMessage('Der Straßenname darf keine Zahlen enthalten.')
+        .isLength({max: 50}),
+
+    body('houseNumber')
+        .trim()
+        .isLength({min: 1, max: 3})
+        .withMessage('Die Hausnummer muss zwischen 1 und 999 sein.')
+        .not()
+        .matches(/\D/)
+        .withMessage('Die Hausnummer darf nur aus Zahlen bestehen.'),
+
+    body('city')
+        .trim()
+        .isLength({max: 32})
+        .withMessage('Der Stadtname darf maximal 32 Zeichen lang sein.')
+        .not()
+        .matches(/\d/)
+        .withMessage('Der stadtname darf keine Zahlen enthalten.'),
+
+    body('zip')
+        .trim()
+        .isLength({min: 5, max: 5})
+        .withMessage('Die PLZ muss 5 Zeichen lang sein.')
+        .not()
+        .matches(/\D/)
+        .withMessage('Die PLZ muss aus Zahlen bestehen.')
+]
