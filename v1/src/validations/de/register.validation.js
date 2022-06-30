2ef8ee31bd235ff2d3ce32eff9747ccc93cbd9db
Jerry <jerry.taag@gmail.com>
2022-06-30 09:42:06 +0200

    fixed validation message

--- v1/src/validations/de/register.validation.js
+++ v1/src/validations/de/register.validation.js
@@ -1,28 +1,29 @@
 import { body } from 'express-validator';
 
 export default [
-    body('email')
-        .not()
-        .isEmpty()
-        .withMessage('Pflichtfeld! Bitte fülle dieses Feld aus.')
-        .trim()
-        .isEmail()
-        .withMessage('Bitte gib eine gültige Email-Adresse an.')
-        .normalizeEmail(),
 
     body('username')
         .not()
         .isEmpty()
-        .withMessage('Pflichtfeld! Bitte fülle dieses Feld aus.')
+        .withMessage('Bitte gib einen enuzernamen an.')
         .trim()
         .isLength({min: 3, max: 50})
         .withMessage('Der Username muss muss zwischen 3 und 50 Zechen lang sein.')
         .blacklist('{[(<@$%>)]}'),
 
+    body('email')
+        .not()
+        .isEmpty()
+        .withMessage('Bitte gib deine Email an.')
+        .trim()
+        .isEmail()
+        .withMessage('Bitte gib eine gültige Email-Adresse an.')
+        .normalizeEmail(),
+
     body('password')
         .not()
         .isEmpty()
-        .withMessage('Pflichtfeld! Bitte fülle dieses Feld aus.')
+        .withMessage('Bitte gib ein Passwort ein.')
         .trim()
         .isLength({min: 10})
         .withMessage('Das Passwort muss mindestens 10 Zeichen lang sein.')
