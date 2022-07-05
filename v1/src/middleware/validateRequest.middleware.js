8956832d29a4e6233397e439c4e627b13f5d6b2a
Dietmar E <86421371+Dima0687@users.noreply.github.com>
2022-07-05 09:07:45 +0200

    Dima: made the err handling a bit better (#46)

--- v1/src/middleware/validateRequest.middleware.js
+++ v1/src/middleware/validateRequest.middleware.js
@@ -9,6 +9,7 @@ const validateRequest = (req, res, next) => {
     next();
   } else {
     const errors = validationErrors.array().reduce((obj, err) => {
+      console.log('validate request',err);
       obj[err.param] = err.msg;
       return obj
     },{});
