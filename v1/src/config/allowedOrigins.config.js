7873dfef395fd4564fca612576dc9a9b36b24289
Dietmar <eis0687.dietmar@gmail.com>
2022-09-09 00:23:07 +0200

    dima: fixed the origin

--- v1/src/config/allowedOrigins.config.js
+++ v1/src/config/allowedOrigins.config.js
@@ -1,5 +1,5 @@
 const allowedOrigins =  [
-  "https://eventlookup-frontend.onrender.com/",
+  "https://eventlookup-frontend.onrender.com",
   "https://eventlookup.netlify.app",
   "https://eventlookup-dev.netlify.app",
   "http://localhost:3000",
